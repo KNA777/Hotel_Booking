8860957af9d1f11a23a4875d4b2588c46612b537
Nikita <vostrotin.dan@yandex.ru>
2025-01-19 23:14:57 +0300

    add any test

--- tests/integrations_tests/bookings/test_db.py
+++ tests/integrations_tests/bookings/test_db.py
@@ -14,35 +14,27 @@ async def test_booking_crud(db):
         date_to=date(2024, 8, 20),
         price=100
     )
-    res = await db.bookings.add(bookings_data)
-    read_res = await db.bookings.get_one_or_none()
-    assert read_res
-    id_bookings = (await db.bookings.get_filtered())[0].id
-    put_bookings_data = BookingsAdd(
+    new_booking = await db.bookings.add(bookings_data)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    # Обновление брони
+    updated_date = date(2024, 9, 20)
+    update_bookings_data = BookingsAdd(
         room_id=room_id,
         user_id=user_id,
         date_from=date(2024, 9, 10),
-        date_to=date(2024, 9, 20),
+        date_to=updated_date,
         price=1000
     )
-    await db.bookings.edit(put_bookings_data, id=id_bookings)
-    result = await db.bookings.get_one_or_none(id=res.id)
-    assert result
-    await db.bookings.delete(id=res.id)
-    res_delete = await db.bookings.get_one_or_none(id=res.id)
-    assert not res_delete
+    await db.bookings.edit(update_bookings_data, id=new_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking
+    assert updated_booking.id == new_booking.id
+    assert updated_booking.date_to == updated_date
 
+    # Удаление брони
 
-
-
-
-
-
-
-
-
-
-
-
-
-    await db.commit()
+    await db.bookings.delete(id=new_booking.id)
+    booking_delete = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not booking_delete
